ed31ab4e319dd9c381a4d8a8c8c0d675898c92b9
lazy34 <maitysurajit0901@gmail.com>
2025-04-22 01:44:57 +0530

    Updated the UI, changes font and fixing layout

---
+++ src/Components/Projects.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Link from 'next/link'; // Make sure to import Link from Next.js
+
+const Projects = () => {
+  const projects = [
+    {
+      title: 'Piplup',
+      date: '2024',
+      about: 'AI integrated Quiz App that enables users to create quizzes using AI and participate in quizzes.',
+      url: 'https://github.com/nextpointer/piplup'
+    },
+    {
+      title: 'Mind Matrix',
+      date: '2023',
+      about: 'Mental Health Management platform that provides tools and resources for mental well-being.',
+      url: 'https://github.com/nextpointer/Mind-Matrix'
+    },
+    {
+      title: 'RYIT',
+      date: '2023',
+      about: 'An educational course platform that offers a variety of online courses for learners.',
+      url: 'https://github.com/nextpointer/RYATM'
+    },
+    {
+      title: 'SplitIt',
+      date: '2024',
+      about: 'A money-splitting app that helps users easily divide expenses among friends and family.',
+      url: 'https://github.com/nextpointer/SplitIt'
+    },
+    {
+      title: 'Mr. Calculator',
+      date: '2022',
+      about: 'A large number calculator designed to perform large calculations with ease.',
+      url: 'https://github.com/nextpointer/Mr.-Calculator'
+    }
+  ];
+
+  return (
+    <>
+      <div className='flex flex-col w-full mt-2'>
+        {projects.map((project, index) => (
+          <div 
+            key={index} 
+            className='flex flex-col w-full mb-4 transition-all duration-200 border-[#6c6c6c] ease-linear hover:border hover:border-[#6c6c6c] hover:text-[#d6d6d6] hover:outline-2 hover:py-2 hover:px-3 rounded-lg' >
+            <div className='flex flex-row w-full justify-between'>
+              <h3 className=''>
+                <Link href={project.url} target="_blank" rel="noopener noreferrer">
+                  {project.title}
+                </Link>
+              </h3>
+              <span>{project.date}</span>
+            </div>
+            <div>
+              <p className='text-sm'>{project.about}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Projects;
