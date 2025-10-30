d104a0295555ab2cd4141b45f8e8d77de8a7a5a0
nextpointer <110530249+nextpointer@users.noreply.github.com>
2025-10-30 12:54:06 +0530

    fix: fix the theme to resolved theme to get resoved system theme

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -6,7 +6,7 @@ import { useTheme } from "next-themes";
 import { Toggle } from "@/components/ui/toggle";
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
   const [mounted, setMounted] = React.useState(false);
 
   React.useEffect(() => {
@@ -15,7 +15,7 @@ export function ModeToggle() {
 
   if (!mounted) return null;
 
-  const isDark = theme === "dark";
+  const isDark = resolvedTheme === "dark";
 
   return (
     <Toggle
